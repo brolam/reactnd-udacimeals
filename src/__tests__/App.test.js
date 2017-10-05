b6c2b6c9fa9071e7e2ca08f2b93cac36a10a9ef0
Breno Marques <breno@brolam.com.br>
2017-10-05 15:36:46 -0300

    feat: started code reducers.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,15 @@
-import 'core-js/es6/map';
-import 'core-js/es6/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
+import { createStore } from 'redux'
+import reducer from '../reducers'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store} />, div);
 });
