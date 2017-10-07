3bce56c96335835e56a9a6d37cade5f07be9eb50
Breno Marques <breno@brolam.com.br>
2017-10-07 12:28:27 -0300

    Complete.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { addRecipe, removeFromCalendar } from '../../actions/index.js'
 
 test('Add a Recipe', () => {
+    console.log(process.env.REACT_APP_APP_KEY);
     expect(addRecipe({ day:{}, recipe:{}, meal:{}})).toEqual({
         type: 'ADD_RECIPE',
         day:{},
